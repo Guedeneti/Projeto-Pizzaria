a35c813cd70861b121296c4a08ef806b61777113
GuedesVPG <65202195+GuedesVPG@users.noreply.github.com>
2020-05-12 19:56:47 -0300

    Delete select.py

--- source/db/select.py
+++
@@ -1,9 +0,0 @@
-"""
-Data.Criacao: 2020-05-010
-Projeto.....: Projeto Pizzaria
-Descricao...: Realizar Pequisa no banco de dados com passagem de parametos, minimizando codigo.
-Arquivo.....: select.py - Pesquisa padrão em tabelas no banco de dados
-Autor.......: Vinicius Guedes
-Observações.: 2020-05-10 - [R00] Criação do Arquivo - Versao 1.00
-              ...
-"""
